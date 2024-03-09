29213aa969b5281a9845c3f992984fb135cd58e7
Ratimon <ratixoxo@gmail.com>
2024-03-09 17:55:08 +0700

    Feat (./script): Abstracting deployer's function

---
+++ script/deployer/DeployerFunctions.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { IDeployer } from "@script/deployer/Deployer.sol";
+
+import {DefaultDeployerFunction, DeployOptions} from "@script/deployer/DefaultDeployerFunction.sol";
+
