caeb1d285713f6c588241194be32d98d1178126a
Rati Montreewat <ratixoxo@gmail.com>
2024-07-29 09:59:16 +0700

    Build (./foundry.toml): Adding config for foundry

--- script/202_DeployPloxyAdmin.s.sol
+++ script/202_DeployPloxyAdmin.s.sol
@@ -6,6 +6,7 @@ import {console2 as console} from "@forge-std/console2.sol";
 import {DeployScript, IDeployer} from "@script/deployer/DeployScript.sol";
 import {DeployerFunctions} from "@script/deployer/DeployerFunctions.sol";
 
+import {AddressManager} from "@main/legacy/AddressManager.sol";
 import {ProxyAdmin} from "@main/universal/ProxyAdmin.sol";
 
 contract DeployProxyAdminScript is DeployScript {
@@ -18,6 +19,15 @@ contract DeployProxyAdminScript is DeployScript {
         uint256 ownerPrivateKey = vm.deriveKey(mnemonic, "m/44'/60'/0'/0/", 1); //  address = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
         owner = vm.envOr("DEPLOYER", vm.addr(ownerPrivateKey));
 
-        return ProxyAdmin(deployer.deploy_ProxyAdmin("ProxyAdmin", address(owner)));
+        ProxyAdmin proxyAdmin = deployer.deploy_ProxyAdmin("ProxyAdmin", address(owner));
+        require(proxyAdmin.owner() == address(owner));
+
+        AddressManager addressManager = AddressManager(deployer.mustGetAddress("AddressManager"));
+
+        if (proxyAdmin.addressManager() != addressManager) {
+            proxyAdmin.setAddressManager(addressManager);
+        }
+
+        return proxyAdmin;
     }
 }
---
+++ script/A00_ShowPrecomputedAddress.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.15;
+
+import {console} from "@forge-std/console.sol";
+
+import {Script} from "@forge-std/Script.sol";
+
+import { Config } from "@script/deployer/Config.sol";
+import { ForgeArtifacts } from "@script/deployer/ForgeArtifacts.sol";
+
+import {Vm} from "forge-std/Vm.sol";
+import { Deployment, IDeployer, getDeployer } from "@script/deployer/Deployer.sol";
+
+contract ShowPrecomputedAddress is Script {
+
+    string internal deploymentOutfile;
+
+    function getAddress() public pure returns (address) {
+        return address(uint160(uint256(keccak256(abi.encode("optimism.deploy")))));
+    }
+
+    function run() public {
+        console.logAddress(getAddress());
+
+        address addr = address(uint160(uint256(keccak256(abi.encode("optimism.deploy")))));
+        Vm vm = Vm(address(bytes20(uint160(uint256(keccak256("hevm cheat code"))))));
+        bytes memory code = vm.getDeployedCode("Deployer.sol:GlobalDeployer");
+
+        console.logBytes(code);
+
+        
+    }
+}
