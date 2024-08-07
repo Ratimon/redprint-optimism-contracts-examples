b1cec18eef0ef0ee32afd02cee6516ec92c3474d
Rati Montreewat <ratixoxo@gmail.com>
2024-08-07 10:06:54 +0700

    Build (./safe-multisig-magamement): Adding contract dependencies

---
+++ safe-multisig-magamement/script/initializeSuperchainConfig.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { console } from "@forge-std/console.sol";
+import { Enum } from "@safe-contracts/Safe.sol";
+import { MultiSend, Utils } from "@script-multisig/Utils.s.sol";
+import "@script-multisig/Constants.s.sol";
+
+contract Example is Utils {
+
+}
