b607207bd4ae428ea0028101531e26a665597002
Rati Montreewat <ratixoxo@gmail.com>
2024-12-09 13:51:30 +0700

    Feat (script): Adding DataAvailabilityChallenge

--- script/300_SetupOpAltDAScript.s.sol
+++ script/300_SetupOpAltDAScript.s.sol
@@ -5,6 +5,7 @@ import {Script} from "@redprint-forge-std/Script.sol";
 import {console} from "@redprint-forge-std/console.sol";
 import {IDeployer, getDeployer} from "@redprint-deploy/deployer/DeployScript.sol";
 import {DeployDataAvailabilityChallengeProxyScript} from "@script/301A_DeployDataAvailabilityChallengeProxyScript.s.sol";
+import {DeployDataAvailabilityChallengeScript} from "@script/301B_DeployDataAvailabilityChallengeScript.s.sol";
 
 contract SetupOpAltDAScript is Script {
     IDeployer deployerProcedue;
@@ -15,10 +16,15 @@ contract SetupOpAltDAScript is Script {
 
         console.log("Setup Op Alt DA ... ");
 
-        DeployDataAvailabilityChallengeProxyScript dataAvailabilityChallengeDeployments = new DeployDataAvailabilityChallengeProxyScript();
+        DeployDataAvailabilityChallengeProxyScript dataAvailabilityChallengeProxyDeployments = new DeployDataAvailabilityChallengeProxyScript();
+        DeployDataAvailabilityChallengeScript dataAvailabilityChallengeDeployments = new DeployDataAvailabilityChallengeScript();
+
+        dataAvailabilityChallengeProxyDeployments.deploy();
         dataAvailabilityChallengeDeployments.deploy();
 
+
         console.log("DataAvailabilityChallengeProxy at: ", deployerProcedue.getAddress("DataAvailabilityChallengeProxy"));
+        console.log("DataAvailabilityChallenge at: ", deployerProcedue.getAddress("DataAvailabilityChallenge"));
 
     }
 }
